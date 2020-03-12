b23e42df414b3c40b6c02cea98612735af25696b
pandhu prisnawan <pandhuprisnawan@gmail.com>
2020-03-12 10:55:52 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -2,24 +2,112 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Carousel } from 'antd';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+
+const { Header, Content, Footer } = Layout;
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+
+const { Dragger } = Upload;
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+const onClick = ({ key }) => {
+  message.info(`Click on item ${key}`);
+};
+
+const menu = (
+  <Menu onClick={onClick}>
+    <Menu.Item key="1">1st menu item</Menu.Item>
+    <Menu.Item key="2">2nd memu item</Menu.Item>
+    <Menu.Item key="3">3rd menu item</Menu.Item>
+  </Menu>
+);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">
+          <div>
+            <Avatar size="small" icon={<UserOutlined />} />
+          </div>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Dropdown overlay={menu}>
+              <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                Dropdown <DownOutlined />
+              </a>
+            </Dropdown>
+          </Menu.Item>
+          <Menu.Item key="3">Kontak</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Carousel</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">
+          <Carousel afterChange={onChange}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+          </Carousel>
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+              band files
+            </p>
+          </Dragger>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
 
